cd0fd1a18b693ed250b917a98573dc661c2811f3
javimangal <javimangal@gmail.com>
2023-11-22 10:53:26 +0100

    Script for the distribution of variables has been updated into a function. I still need to work on documentation of the function. Minor changes were made to the Part 2 file.

--- R/distribution_numerical_variables.R
+++ R/distribution_numerical_variables.R
@@ -1,24 +1,32 @@
-for (variable in names(data)) {
-  if (is.numeric(data[[variable]])) {
-    name <- variable
-
-    Main <- paste("Histogram of", variable, sep = " ")
-    hist(data[[variable]],
-      xlab = name, main = Main,
-      col = "steelblue"
-    )
-
-    Main <- paste("Boxplot of", variable, sep = " ")
-    boxplot(data[[variable]],
-      xlab = name, main = Main,
-      horizontal = TRUE,
-      col = "steelblue"
-    )
-
-    Main <- paste("Normal Q-Q Plot of", variable, sep = " ")
-    qqnorm(data[[variable]], main = Main)
-    qqline(data[[variable]], col = "steelblue", lwd = 2)
+distribution_plots <- function(data){
+  #' @title Distribution plots
+  #' @description This function takes every  
+  #' numeric variable in a dataframe 
+  #' and creates histograms, boxplots, 
+  #' and Q-Q plots.  
+  #' @param data The first value
+  #' @return The two arguments multiplied.
+  for (variable in names(data)) {
+    if (is.numeric(data[[variable]])) {
+      name <- variable
+      
+      Main <- paste("Histogram of", variable, sep = " ")
+      hist(data[[variable]],
+           xlab = name, main = Main,
+           col = "steelblue"
+      )
+      
+      Main <- paste("Boxplot of", variable, sep = " ")
+      boxplot(data[[variable]],
+              xlab = name, main = Main,
+              horizontal = TRUE,
+              col = "steelblue"
+      )
+      
+      Main <- paste("Normal Q-Q Plot of", variable, sep = " ")
+      qqnorm(data[[variable]], main = Main)
+      qqline(data[[variable]], col = "steelblue", lwd = 2)
+    }
   }
 }
 
-rm(variable, Main, name)
