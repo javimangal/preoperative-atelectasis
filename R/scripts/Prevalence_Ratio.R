6cc7deb9b9f2129b107bb24e8c476fcf44ab279c
javimangal <javimangal@gmail.com>
2023-11-28 23:45:38 +0100

    Changes in table2 for prevalence rations since priorly the models were not ran with the structure of a 2x2 table. Univariable models did not change, whereas there were some changes in adjusted prevalence ratios. Direction of results did not change, but prior models overestimated the effect and current models reflect uncertainty better now and seem to be more realistic now.

--- R/scripts/Prevalence_Ratio.R
+++ R/scripts/Prevalence_Ratio.R
@@ -1,4 +1,4 @@
-## Crude prevalence ratio
+## Crude prevalence ratio Class 2 Obesity  
 
 # Prepare variable by using 0 and 1 as required by the model code:
 dataprev <- data %>%
@@ -11,9 +11,11 @@ dataprev <- data %>%
     )
   ))
 
+# Prepare subset of BMI class 1 and 2:   
+dataprev_1 <- dataprev %>% filter(type_obesity == "Class 1 Obesity" | type_obesity == "Class 2 Obesity")
 
 poisson_fit <- glm(atelectasis ~ type_obesity,
-  data = dataprev,
+  data = dataprev_1,
   family = poisson(link = log)
 )
 
@@ -32,7 +34,7 @@ se <- sqrt(diag(covmat))
 #  2. robust standard errors
 #  3. 95% confidence intervals
 # Note that qnorm(0.975) approximately equals 1.96
-model_output <- cbind(
+model_output_prev_1 <- cbind(
   Estimate = exp(coef(poisson_fit)),
   `Robust SE` = se,
   Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
@@ -40,14 +42,14 @@ model_output <- cbind(
 )
 
 # Coerce model_output into a data frame
-model_output <- as.data.frame(round(model_output, 2))
-model_output
+model_output_prev_1 <- as.data.frame(round(model_output_prev_1, 2))
+model_output_prev_1
 
 
 
-## Adjusted prevalence ratio
+## Adjusted prevalence ratio  Class 2 Obesity
 poisson_fit <- glm(atelectasis ~ type_obesity + age + sex + sleep_apnea,
-  data = dataprev,
+  data = dataprev_1,
   family = poisson(link = log)
 )
 
@@ -65,7 +67,7 @@ se <- sqrt(diag(covmat))
 #  2. robust standard errors
 #  3. 95% confidence intervals
 # Note that qnorm(0.975) approximately equals 1.96
-model_output2 <- cbind(
+model_output_prev_1_adj <- cbind(
   Estimate = exp(coef(poisson_fit)),
   `Robust SE` = se,
   Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
@@ -73,26 +75,117 @@ model_output2 <- cbind(
 )
 
 # Coerce model_output into a data frame
-model_output2 <- as.data.frame(round(model_output2, 2))
-model_output2
+model_output_prev_1_adj <- as.data.frame(round(model_output_prev_1_adj, 2))
+model_output_prev_1_adj
+
+
+
+## Crude prevalence ratio Class 3 Obesity  
+
+# Prepare subset of BMI class 1 and 3:   
+dataprev_2 <- dataprev %>% filter(type_obesity == "Class 1 Obesity" | type_obesity == "Class 3 Obesity")
+
+poisson_fit <- glm(atelectasis ~ type_obesity,
+                   data = dataprev_2,
+                   family = poisson(link = log)
+)
+
+tidy(poisson_fit, exponentiate = TRUE, conf.int = TRUE)
+
+
+# Robust standard errors:
+covmat <- vcovHC(poisson_fit, type = "HC0")
+covmat
+
+# Calculate the standard error
+se <- sqrt(diag(covmat))
+
+# Bind together model output
+#  1. exponentiated coefficients
+#  2. robust standard errors
+#  3. 95% confidence intervals
+# Note that qnorm(0.975) approximately equals 1.96
+model_output_prev_2 <- cbind(
+  Estimate = exp(coef(poisson_fit)),
+  `Robust SE` = se,
+  Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
+  Upper = exp(coef(poisson_fit) + qnorm(0.975) * se)
+)
+
+# Coerce model_output into a data frame
+model_output_prev_2 <- as.data.frame(round(model_output_prev_2, 2))
+model_output_prev_2
+
+
+
+## Adjusted prevalence ratio  Class 2 Obesity
+poisson_fit <- glm(atelectasis ~ type_obesity + age + sex + sleep_apnea,
+                   data = dataprev_2,
+                   family = poisson(link = log)
+)
+
+tidy(poisson_fit, exponentiate = TRUE, conf.int = TRUE)
+
+# Robust standard errors:
+covmat <- vcovHC(poisson_fit, type = "HC0")
+covmat
+
+# Calculate the standard error
+se <- sqrt(diag(covmat))
+
+# Bind together model output
+#  1. exponentiated coefficients
+#  2. robust standard errors
+#  3. 95% confidence intervals
+# Note that qnorm(0.975) approximately equals 1.96
+model_output_prev_2_adj <- cbind(
+  Estimate = exp(coef(poisson_fit)),
+  `Robust SE` = se,
+  Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
+  Upper = exp(coef(poisson_fit) + qnorm(0.975) * se)
+)
+
+# Coerce model_output into a data frame
+model_output_prev_2_adj <- as.data.frame(round(model_output_prev_2_adj, 2))
+model_output_prev_2_adj
 
 
 ## Table 2
-table_output <- model_output %>%
-  dplyr::slice(2:3) %>%
+# Merge output table for unadjusted models:   
+table_output_prev_1 <- model_output_prev_1 %>%
+  dplyr::slice(2) %>%
   rename(PR = Estimate, SE = `Robust SE`) %>%
   mutate("95%CI" = paste(Lower, Upper, sep = "-")) %>%
   select(-c(Lower, Upper))
 
-table_output2 <- model_output2 %>%
-  dplyr::slice(2:3) %>%
+table_output_prev_2 <- model_output_prev_2 %>%
+  dplyr::slice(2) %>%
+  rename(PR = Estimate, SE = `Robust SE`) %>%
+  mutate("95%CI" = paste(Lower, Upper, sep = "-")) %>%
+  select(-c(Lower, Upper))
+
+table_output <- rbind.data.frame(table_output_prev_1,
+                                 table_output_prev_2)
+
+# Merge output table for adjusted models:  
+table_output_prev_1_adj <- model_output_prev_1_adj %>%
+  dplyr::slice(2) %>%
   rename(aPR = Estimate, aSE = `Robust SE`) %>%
   mutate("a95%CI" = paste(Lower, Upper, sep = "-")) %>%
   select(-c(Lower, Upper))
+
+table_output_prev_2_adj <- model_output_prev_2_adj %>%
+  dplyr::slice(2) %>%
+  rename(aPR = Estimate, aSE = `Robust SE`) %>%
+  mutate("a95%CI" = paste(Lower, Upper, sep = "-")) %>%
+  select(-c(Lower, Upper))
+
+table_output2 <- rbind.data.frame(table_output_prev_1_adj,
+                                  table_output_prev_2_adj)
 
 table2 <- dplyr::bind_cols(table_output, table_output2) %>%
   rownames_to_column(var = "Category") %>%
   mutate_at("Category", str_replace, "type_obesity", "")
 
 flextable(table2) %>%
-  save_as_docx(path = paste(tabfolder, "/Table2.docx", sep = ""))
+  save_as_docx(path = paste0(tabfolder, "/Table2.docx"))
--- R/scripts/Prevalence_Ratio_subgroups.R
+++ R/scripts/Prevalence_Ratio_subgroups.R
@@ -1,20 +1,22 @@
-## Crude prevalence ratio
+## Crude prevalence ratio BMI 40-45 
 
 # Prepare variable by using 0 and 1 as required by the model code:
 dataprev <- data %>%
   mutate(atelectasis = as.numeric(
     as.character(
       fct_recode(atelectasis,
-        "0" = "No",
-        "1" = "Yes"
+                 "0" = "No",
+                 "1" = "Yes"
       )
     )
   ))
 
+# Prepare subset of BMI class 1 and 2:   
+dataprev_1 <- dataprev %>% filter(type_obesity == "30-35" | type_obesity == "40-45")
 
 poisson_fit <- glm(atelectasis ~ type_obesity,
-  data = dataprev,
-  family = poisson(link = log)
+                   data = dataprev_1,
+                   family = poisson(link = log)
 )
 
 tidy(poisson_fit, exponentiate = TRUE, conf.int = TRUE)
@@ -32,7 +34,7 @@ se <- sqrt(diag(covmat))
 #  2. robust standard errors
 #  3. 95% confidence intervals
 # Note that qnorm(0.975) approximately equals 1.96
-model_output <- cbind(
+model_output_prev_1 <- cbind(
   Estimate = exp(coef(poisson_fit)),
   `Robust SE` = se,
   Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
@@ -40,15 +42,15 @@ model_output <- cbind(
 )
 
 # Coerce model_output into a data frame
-model_output <- as.data.frame(round(model_output, 2))
-model_output
+model_output_prev_1 <- as.data.frame(round(model_output_prev_1, 2))
+model_output_prev_1
 
 
 
-## Adjusted prevalence ratio
+## Adjusted prevalence ratio  Class 2 Obesity
 poisson_fit <- glm(atelectasis ~ type_obesity + age + sex + sleep_apnea,
-  data = dataprev,
-  family = poisson(link = log)
+                   data = dataprev_1,
+                   family = poisson(link = log)
 )
 
 tidy(poisson_fit, exponentiate = TRUE, conf.int = TRUE)
@@ -65,7 +67,7 @@ se <- sqrt(diag(covmat))
 #  2. robust standard errors
 #  3. 95% confidence intervals
 # Note that qnorm(0.975) approximately equals 1.96
-model_output2 <- cbind(
+model_output_prev_1_adj <- cbind(
   Estimate = exp(coef(poisson_fit)),
   `Robust SE` = se,
   Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
@@ -73,23 +75,204 @@ model_output2 <- cbind(
 )
 
 # Coerce model_output into a data frame
-model_output2 <- as.data.frame(round(model_output2, 2))
-model_output2
+model_output_prev_1_adj <- as.data.frame(round(model_output_prev_1_adj, 2))
+model_output_prev_1_adj
+
+
+
+## Crude prevalence ratio BMI 45-50  
+
+# Prepare subset of BMI class 1 and 3:   
+dataprev_2 <- dataprev %>% filter(type_obesity == "30-35" | type_obesity == "45-50")
+
+poisson_fit <- glm(atelectasis ~ type_obesity,
+                   data = dataprev_2,
+                   family = poisson(link = log)
+)
+
+tidy(poisson_fit, exponentiate = TRUE, conf.int = TRUE)
+
+
+# Robust standard errors:
+covmat <- vcovHC(poisson_fit, type = "HC0")
+covmat
+
+# Calculate the standard error
+se <- sqrt(diag(covmat))
+
+# Bind together model output
+#  1. exponentiated coefficients
+#  2. robust standard errors
+#  3. 95% confidence intervals
+# Note that qnorm(0.975) approximately equals 1.96
+model_output_prev_2 <- cbind(
+  Estimate = exp(coef(poisson_fit)),
+  `Robust SE` = se,
+  Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
+  Upper = exp(coef(poisson_fit) + qnorm(0.975) * se)
+)
+
+# Coerce model_output into a data frame
+model_output_prev_2 <- as.data.frame(round(model_output_prev_2, 2))
+model_output_prev_2
+
+
+
+## Adjusted prevalence ratio  Class 2 Obesity
+poisson_fit <- glm(atelectasis ~ type_obesity + age + sex + sleep_apnea,
+                   data = dataprev_2,
+                   family = poisson(link = log)
+)
+
+tidy(poisson_fit, exponentiate = TRUE, conf.int = TRUE)
+
+# Robust standard errors:
+covmat <- vcovHC(poisson_fit, type = "HC0")
+covmat
+
+# Calculate the standard error
+se <- sqrt(diag(covmat))
+
+# Bind together model output
+#  1. exponentiated coefficients
+#  2. robust standard errors
+#  3. 95% confidence intervals
+# Note that qnorm(0.975) approximately equals 1.96
+model_output_prev_2_adj <- cbind(
+  Estimate = exp(coef(poisson_fit)),
+  `Robust SE` = se,
+  Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
+  Upper = exp(coef(poisson_fit) + qnorm(0.975) * se)
+)
+
+# Coerce model_output into a data frame
+model_output_prev_2_adj <- as.data.frame(round(model_output_prev_2_adj, 2))
+model_output_prev_2_adj
+
+
+## Crude prevalence ratio BMI >50    
+
+# Prepare subset of BMI class 1 and 3:   
+dataprev_3 <- dataprev %>% filter(type_obesity == "30-35" | type_obesity == "≥50")
+
+poisson_fit <- glm(atelectasis ~ type_obesity,
+                   data = dataprev_3,
+                   family = poisson(link = log)
+)
+
+tidy(poisson_fit, exponentiate = TRUE, conf.int = TRUE)
+
+
+# Robust standard errors:
+covmat <- vcovHC(poisson_fit, type = "HC0")
+covmat
+
+# Calculate the standard error
+se <- sqrt(diag(covmat))
+
+# Bind together model output
+#  1. exponentiated coefficients
+#  2. robust standard errors
+#  3. 95% confidence intervals
+# Note that qnorm(0.975) approximately equals 1.96
+model_output_prev_3 <- cbind(
+  Estimate = exp(coef(poisson_fit)),
+  `Robust SE` = se,
+  Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
+  Upper = exp(coef(poisson_fit) + qnorm(0.975) * se)
+)
+
+# Coerce model_output into a data frame
+model_output_prev_3 <- as.data.frame(round(model_output_prev_3, 2))
+model_output_prev_3
+
+
+
+## Adjusted prevalence ratio  Class 2 Obesity
+poisson_fit <- glm(atelectasis ~ type_obesity + age + sex + sleep_apnea,
+                   data = dataprev_3,
+                   family = poisson(link = log)
+)
+
+tidy(poisson_fit, exponentiate = TRUE, conf.int = TRUE)
+
+# Robust standard errors:
+covmat <- vcovHC(poisson_fit, type = "HC0")
+covmat
+
+# Calculate the standard error
+se <- sqrt(diag(covmat))
+
+# Bind together model output
+#  1. exponentiated coefficients
+#  2. robust standard errors
+#  3. 95% confidence intervals
+# Note that qnorm(0.975) approximately equals 1.96
+model_output_prev_3_adj <- cbind(
+  Estimate = exp(coef(poisson_fit)),
+  `Robust SE` = se,
+  Lower = exp(coef(poisson_fit) - qnorm(0.975) * se),
+  Upper = exp(coef(poisson_fit) + qnorm(0.975) * se)
+)
+
+# Coerce model_output into a data frame
+model_output_prev_3_adj <- as.data.frame(round(model_output_prev_3_adj, 2))
+model_output_prev_3_adj
 
 
 ## Table 2
-table_output <- model_output %>%
-  dplyr::slice(2:5) %>%
+# Merge output table for unadjusted models:   
+table_output_prev_1 <- model_output_prev_1 %>%
+  dplyr::slice(2) %>%
   rename(PR = Estimate, SE = `Robust SE`) %>%
   mutate("95%CI" = paste(Lower, Upper, sep = "-")) %>%
   select(-c(Lower, Upper))
 
-table_output2 <- model_output2 %>%
-  dplyr::slice(2:5) %>%
+table_output_prev_2 <- model_output_prev_2 %>%
+  dplyr::slice(2) %>%
+  rename(PR = Estimate, SE = `Robust SE`) %>%
+  mutate("95%CI" = paste(Lower, Upper, sep = "-")) %>%
+  select(-c(Lower, Upper))
+
+table_output_prev_3 <- model_output_prev_3 %>%
+  dplyr::slice(2) %>%
+  rename(PR = Estimate, SE = `Robust SE`) %>%
+  mutate("95%CI" = paste(Lower, Upper, sep = "-")) %>%
+  select(-c(Lower, Upper))
+
+
+table_output <- rbind.data.frame(
+  table_output_prev_1,
+  table_output_prev_2,
+  table_output_prev_3
+  )
+
+# Merge output table for adjusted models:  
+table_output_prev_1_adj <- model_output_prev_1_adj %>%
+  dplyr::slice(2) %>%
   rename(aPR = Estimate, aSE = `Robust SE`) %>%
   mutate("a95%CI" = paste(Lower, Upper, sep = "-")) %>%
   select(-c(Lower, Upper))
 
+table_output_prev_2_adj <- model_output_prev_2_adj %>%
+  dplyr::slice(2) %>%
+  rename(aPR = Estimate, aSE = `Robust SE`) %>%
+  mutate("a95%CI" = paste(Lower, Upper, sep = "-")) %>%
+  select(-c(Lower, Upper))
+
+table_output_prev_3_adj <- model_output_prev_3_adj %>%
+  dplyr::slice(2) %>%
+  rename(aPR = Estimate, aSE = `Robust SE`) %>%
+  mutate("a95%CI" = paste(Lower, Upper, sep = "-")) %>%
+  select(-c(Lower, Upper))
+
+table_output2 <- rbind.data.frame(
+  table_output_prev_1_adj,
+  table_output_prev_2_adj,
+  table_output_prev_3_adj
+  )
+
+# Merge two tables   
 table2 <- dplyr::bind_cols(table_output, table_output2) %>%
   rownames_to_column(var = "Category") %>%
   mutate_at("Category", str_replace, "type_obesity", "")
